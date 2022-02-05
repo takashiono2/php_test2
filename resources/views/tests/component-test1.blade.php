cda33797783ec843467ae830d53943fe814dc5ee
takashiono2 <takashiono710@gmail.com>
2022-02-05 18:32:29 +0900

    属性バック

--- resources/views/tests/component-test1.blade.php
+++ resources/views/tests/component-test1.blade.php
@@ -3,4 +3,5 @@
     コンポーネント1
     <x-tests.card title="タイトル1" content="本文1" :message="$message" />
     <x-tests.card title="タイトル2"/>
+    <x-tests.card title="cssを変更したい" class="bg-red-300"/>
 </x-tests.app>
