cae8b1933918666cee79fe79032644b3c3404c7c
takashiono2 <takashiono710@gmail.com>
2022-02-13 19:24:55 +0900

    indexとcreate

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -20,12 +20,12 @@ class OwnersController extends Controller{
      * @return \Illuminate\Http\Response
      */
     public function index(){
-        $date_now = Carbon::now();
-        $date_parse = Carbon::parse(now());
+        // $date_now = Carbon::now();
+        // $date_parse = Carbon::parse(now());
         // echo $date_now->year;
         // echo $date_parse;
-        $e_all = Owner::all();
-        $q_get = DB::table('owners')->select('name','created_at')->get();
+        // $e_all = Owner::all();
+        // $q_get = DB::table('owners')->select('name','created_at')->get();
         // $q_first = DB::table('owners')->select('name')->first();
 
         // $c_test = collect([
@@ -33,7 +33,9 @@ class OwnersController extends Controller{
         // ]);
 
         // dd($e_all,$q_get,$q_first,$c_test);
-        return view('admin.owners.index',compact('e_all','q_get'));
+        $owners = Owner::select('name','email','created_at')->get();
+
+        return view('admin.owners.index',compact('owners'));
     }
 
     /**
@@ -41,9 +43,8 @@ class OwnersController extends Controller{
      *
      * @return \Illuminate\Http\Response
      */
-    public function create()
-    {
-        //
+    public function create(){
+        return view('admin.owners.create');
     }
 
     /**
