e94bf43affafeadce8013011f146169f4dcc2957
takashiono2 <takashiono710@gmail.com>
2022-02-26 19:49:57 +0900

    shoppadd

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(){
         $this->call([
             OwnerSeeder::class,
-            AdminSeeder::class
+            AdminSeeder::class,
+            ShopSeeder::class
         ]);
     }
 }
